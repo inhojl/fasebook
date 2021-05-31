22c57b02306fae7f84d0f93057b8707a37fb4c28
Inho Lee <inhojl@outlook.com>
2021-05-31 10:16:52 -0700

    implemented application, users, sessions, and static pages controller, added routes, created root static page with bootstrapping current user to window

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,38 @@
 class Api::UsersController < ApplicationController
+
+  def show
+    
+  end
+
+  def index
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, 
+      :password, 
+      :first_name, 
+      :last_name, 
+      :birthdate, 
+      :gender_id
+    )
+  end
+
 end
